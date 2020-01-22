33187667c336fb84c6aca14b0d5a6986f62f7246
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2020-01-22 11:27:43 +0100

    working extraction

--- atlas/atlas_scraping.py
+++ atlas/atlas_scraping.py
@@ -1,6 +1,6 @@
 # %%
 # PDF handling
-import PyPDF2
+import PyPDF4 as pypdf
 from PIL import Image
 # Regex
 import re
@@ -8,7 +8,7 @@ import re
 import matplotlib.pyplot as plt
 
 # %%
-input1 = PyPDF2.PdfFileReader(open("./data/atlas_LIST.pdf", "rb"))
+input1 = pypdf.PdfFileReader(open("./data/atlas_LIST.pdf", "rb"))
 taxon_regex = "[A-Z]{4}"
 taxons = {}
 for section in input1.getOutlines():
@@ -22,29 +22,46 @@ for section in input1.getOutlines():
 taxons = {k: v for k, v in sorted(taxons.items(), key=lambda item: item[1])}
 
 # %%
+def extractImages(xObject, name):
+    keys=[obj[0] for obj in xObject.items()]
+    page_images = []
+    i = 0
+    for obj in keys:
+        if xObject[obj]["/Subtype"] == "/Image":
+            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+            data = xObject[obj].getData()
+
+            # Setting mode
+            if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                mode = "RGB"
+            else:
+                mode = "P"
+
+            image_name = "./tmp/"+ name + "_" + str(i)
+            # Decoding
+            if xObject[obj]['/Filter'] == '/FlateDecode':
+                img = Image.frombytes(mode, size, data)
+                img.save(image_name + ".png")
+            elif xObject[obj]['/Filter'] == '/DCTDecode':
+                img = open(image_name + ".jpg", "wb")
+                img.write(data)
+                img.close()
+            elif xObject[obj]['/Filter'] == '/JPXDecode':
+                img = open(image_name + ".jp2", "wb")
+                img.write(data)
+                img.close()
+            i+=1
+
 key_list = list(taxons.keys())
 for i, name in enumerate(key_list):
     n_start = taxons[key_list[i]]
     n_end = taxons[key_list[i+1]]-1
     for page_num in range(n_start, n_end+1):
+        print(name, page_num)
         page = input1.getPage(page_num)
         xObject = page['/Resources']['/XObject'].getObject()
-
-# %%
-keys=[obj[0] for obj in xObject.items()]
-page_images = []
-for obj in keys:
-    size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-    data = xObject[obj].getData()
-    if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-        mode = "RGB"
-    else:
-        mode = "P"
-    page_images.append(Image.frombytes(mode, size, data))
-for page_images)
+        extractImages(xObject, name)
 
 # %%
 plt.imshow(img)
 # img.save(obj[1:] + ".png")
-
-# %%
