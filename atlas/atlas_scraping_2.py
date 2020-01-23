5b31389281b5228fa06aba48c77ff30633540d18
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2020-01-23 14:51:14 +0100

    k

--- atlas/atlas_scraping_2.py
+++ atlas/atlas_scraping_2.py
@@ -1,20 +1,30 @@
 # %%
 # PDF handling
-import fitz
+import fitz # pip install PyMuPDF
 # Regex
 import re
 # Plotting
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import os
 
 def pix2np(pix):
     im = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
     im = np.ascontiguousarray(im[..., [2, 1, 0]])  # rgb to bgr
     return im
 
+def showImg(img, scale=1):
+    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', scale*img.shape[1], scale*img.shape[0])
+    cv2.imshow('image',img)
+    k = cv2.waitKey(0)
+    print("DEBUG: waitKey returned:", chr(k))
+    cv2.destroyAllWindows()
+
 # %%
-input1 = fitz.open("./data/atlas_LIST_uncompressed.pdf")
+pdf_path = "./data/atlas_LIST.pdf"
+input1 = fitz.open(pdf_path)
 taxon_regex = "[A-Z]{4}"
 taxons = {}
 for section in input1.getToC():
@@ -27,27 +37,6 @@ for section in input1.getToC():
     if match and page_num != (-1):
         taxons[match.group()] = page_num
 taxons = {k: v for k, v in sorted(taxons.items(), key=lambda item: item[1])}
-
-# %%
-key_list = list(taxons.keys())
-for i, name in enumerate(key_list):
-    n_start = taxons[key_list[i]]
-    n_end = taxons[key_list[i+1]]-1
-    for page_num in range(n_start, n_end+1):
-        print(name, page_num)
-        i = 0
-        for img in input1.getPageImageList(page_num):
-            image_name = "./tmp/"+ name + "_" + str(i) + ".png"
-            xref = img[0]
-            pix = fitz.Pixmap(input1, xref)
-            if pix.n < 5:       # this is GRAY or RGB
-                pix.writePNG(image_name)
-            else:               # CMYK: convert to RGB first
-                pix1 = fitz.Pixmap(fitz.csRGB, pix)
-                pix1.writePNG(image_name)
-                pix1 = None
-            pix = None
-            i += 1
 
 # %%
 key_list = list(taxons.keys())
@@ -70,9 +59,57 @@ for i, name in enumerate(key_list):
             pix = None
         i = 0
         for size in images:
-            image_name = "./tmp/"+ name + "_" + str(i) + ".png"
+            image_name = "./tmp0/"+ name + "/" + str(i) + "_"
             tmp = np.vstack(images[size])
-            cv2.imwrite(image_name,tmp)
+            handleImage(tmp, "tmp0", name)
             i += 1
         
         #cv morpho ->  conected components -> bounding box -> 
+
+# %%
+def handleImage(img, root, folder):
+# img = cv2.imread('./tmp/AAMB_0.png',cv2.IMREAD_UNCHANGED)
+    height, width = img.shape[0], img.shape[0]
+    img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret,img_thr = cv2.threshold(img_gs,230,255,cv2.THRESH_BINARY_INV)
+    #OPENING
+    kernel_size = 20
+    kernel = np.ones((kernel_size,kernel_size),np.uint8)
+    img_thr = cv2.morphologyEx(img_thr, cv2.MORPH_OPEN, kernel)
+    #CONNECTED COMPONENTS
+    output = cv2.connectedComponentsWithStats(img_thr)
+    num_labels = output[0]
+    labels = output[1]
+    stats = output[2]
+    centroids = output[3]
+    j = 0
+    for i in range(num_labels):
+        area = stats[i][cv2.CC_STAT_AREA]
+        x, y = stats[i][cv2.CC_STAT_LEFT], stats[i][cv2.CC_STAT_TOP]
+        w, h = stats[i][cv2.CC_STAT_WIDTH], stats[i][cv2.CC_STAT_HEIGHT]
+        if area > height*width*0.01:
+            ROI = img[y:y+h,x:x+w]
+            # showImg(ROI)
+            j+=1
+            path_to_folder = "./"+root+"/"+name
+            full_path = path_to_folder+"/"+str(j)+".png"
+            print(path_to_folder,full_path)
+            if not os.path.exists(path_to_folder):
+                os.makedirs(path_to_folder)
+            cv2.imwrite(full_path,ROI)
+    print(j, "component(s) extracted!")
+
+# %%
+ret,img_thr = cv2.threshold(img_gs,20,255,cv2.THRESH_BINARY_INV)
+print(img_thr.shape)
+showImg(img_thr)
+contours,h = cv2.findContours(img_thr,1,2)
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+    print(len(approx))
+    if len(approx)==4:
+        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# %%
