b20f0b7c84d54d1ba61956f4fba3c0b193a7e096
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2020-02-03 09:37:22 +0100

    k

--- artifical_dataset/main.py
+++ artifical_dataset/main.py
@@ -1,6 +1,6 @@
 # %%
 import cv2
-import numpy
+import numpy as np
 import random
 
 from os import listdir
@@ -13,11 +13,22 @@ images = [f for f in listdir(DATASET_PATH) if isfile(join(DATASET_PATH, f))]
 # %% LOADING n RANDOM images
 n = random.randint(7,10)
 tmp_images = []
+mean_brightness = 0
 for i in range(n):
-    tmp = random.choice(images)
-    while(len(a.split('.'))<2 or tmp.split('.')[1]!='png'):
-        tmp = random.choice(images)
-    tmp_images.append(tmp)
+    # CHOOSING RANDOM IMAGE
+    img_path = random.choice(images)
+    while(len(img_path.split('.'))<2 or img_path.split('.')[1]!='png'):
+        img_path = random.choice(images)
+    # LOADING THE IMAGE
+    img_path = join(DATASET_PATH, img_path)
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    mean_brightness += np.mean(img)
+    tmp_images.append(img)
+mean_brightness /= len(tmp_images)
+
+# %% NORMALIZING BRIGHTNESS
+for img in tmp_images:
+    img += (mean_brightness-np.mean(img)).astype(np.uint8)
+    img[img>255] = 255
 
 # %%
-img_path = join(DATASET_PATH, f)
