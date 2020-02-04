3a912d0c5e676ced508ac5575163344fc52660f3
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2020-02-04 14:09:54 +0100

    k

--- artifical_dataset/main.py
+++ artifical_dataset/main.py
@@ -79,17 +79,24 @@ showImg(art_img)
 showImg(global_patch_mask)
 
 # %%
-final_img = global_patch.copy()
-showImg(final_img)
 # Constructing kd tree with known values
 known = np.argwhere(global_patch_mask!=0)
 kdt = KDTree(known, leaf_size=30, metric='euclidean')
 # Finding neirest neighbors of unknownn values
 unknown = np.argwhere(global_patch_mask==0)
-nn = kdt.query(unknown, k=5, return_distance=False)
+nn_query = kdt.query(unknown, k=10, return_distance=True)
+nn = nn_query[1]
+nn_weights = nn_query[0]
 # Filling
+
+# %%
+final_img = global_patch.copy()
+showImg(final_img)
 for i in range(len(unknown)):
-    final_img[unknown[i][0], unknown[i][1]] = np.mean(global_patch[unknown[nn[i]][0], unknown[nn[i]][1]]).astype(np.uint8)
+    x_u, y_u = unknown[i][0], unknown[i][1] 
+    avg_values = global_patch[known[nn[i]][:,0], known[nn[i]][:,1]]
+    # final_img[x_u, y_u] = np.mean(global_patch[known[nn[i]][:,0], known[nn[i]][:,1]]).astype(np.uint8)
+    final_img[x_u, y_u] = np.average(avg_values, weights=nn_weights[i]).astype(np.uint8)
 showImg(final_img)
 
 # %%
