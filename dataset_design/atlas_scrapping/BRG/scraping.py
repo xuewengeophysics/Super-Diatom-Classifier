b476170c00c890b1a12714bc6ced2ca4c6b07793
PierreFG <pierre.faure.giovagnoli@gmail.com>
2020-04-13 17:06:36 +0200

    k

--- dataset_design/atlas_scrapping/BRG/scraping.py
+++ dataset_design/atlas_scrapping/BRG/scraping.py
@@ -37,87 +37,45 @@ def saveImg(img, root, folder, id):
     cv2.imwrite(full_path,img)
 
 # %%
-def handlePDF(doc):
-    taxon_regex = "(?:^|\W)[A-Z]{4}(?:^|\W)"
-    p = re.compile(taxon_regex)
-    for i in range(20, len(doc)):
-        page = doc.loadPage(i)
-        text = page.getText("text")
-        match = p.search(text)
-        if match and match.group().find("TOME")==-1:
-            print("---------------------------------")
-            print(page)
-            print(match.group(), page.number)
-            taxon = ''.join(e for e in match.group() if e.isalnum())
-            i = 0
-            for img in doc.getPageImageList(page.number):
-                xref = img[0]
-                pix = fitz.Pixmap(doc, xref)
-                if pix.n >=5:        # CMYK: convert to RGB first
-                    pix = fitz.Pixmap(fitz.csRGB, pix)
-                img = pix2np(pix)
-                if img.shape[0]>10 and img.shape[1]>10:
-                    area = img.shape[0]*img.shape[1]
-                    if img.shape[2]!=1:
-                        img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    ret,img_thr = cv2.threshold(img_gs,254,255,cv2.THRESH_BINARY)
-                    n = len(np.where(img_thr==255)[0])
-                    if n<area*0.2:
-                        saveImg(img_gs, "tmp", taxon, i)
-                        i+=1
-
-
-# %%
-root_path = "./data/"
+root_path = "/media/pierrefg/01D341A80E60C350/Users/pierr/Google Drive/School/Georgia Tech/Super Diatomee Classifier/Atlas/PDFs/BRG (LIST)/"
 pdfs = [f for f in listdir(root_path) if isfile(join(root_path, f))]
-for atlas_path in pdfs:
-    split = atlas_path.split(".")
-    if split[1]=='pdf':
-        doc = fitz.open(root_path+atlas_path)
-        taxon = split[0]
-        for img in doc.getPageImageList(1):
-            if img[7] == "I5":
-                xref = img[0]
-                print(img)
-                pix = fitz.Pixmap(doc, xref)
-                if pix.n >=5:        # CMYK: convert to RGB first
-                    pix = fitz.Pixmap(fitz.csRGB, pix)
-                img = pix2np(pix)
-                # showImg(img)
-                saveImg(img, "tmp", None, taxon)
-
+pdf_path = os.path.join(root_path, pdfs[3])
+print("Opening: ", pdf_path)
+doc = fitz.open(pdf_path)
 
 # %%
-# img = cv2.imread('./tmp/AAMB.png',cv2.IMREAD_UNCHANGED)
-root_path = "./tmp/"
-imgs = [f for f in listdir(root_path) if isfile(join(root_path, f))]
-for img_name in imgs:
-    split = img_name.split(".")
-    if split[1]=='png':
-        img = cv2.imread(root_path+img_name,cv2.IMREAD_UNCHANGED)
-        taxon = split[0]
-        print(taxon)
-        height, width = img.shape[0], img.shape[1]
-        img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img_comp = cv2.cvtColor(img_gs, cv2.COLOR_GRAY2BGR)
-        # Finding common parts
-        sub = img-img_comp
-        sub_thr = cv2.inRange(sub, (0,0,0),(1,1,1))
-        kernel_size = 3
-        kernel = np.ones((kernel_size,kernel_size),np.uint8)
-        mask = cv2.morphologyEx(sub_thr, cv2.MORPH_CLOSE, kernel)
-        # showImg(img_thr)
-
-        contours,h = cv2.findContours(mask,1,2)
-        # showImg(img_thr)
-        j=0
-        padding = 1
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            if w*h > height*width*0.005 and w*h<height*width*0.95:
-                ROI = img_gs[y-padding:y+h+padding,x-padding:x+w+padding]
-                if ROI.shape[0]>10 and ROI.shape[1]>10:
-                    saveImg(ROI, "tmp0", taxon, j)
-                    j+=1
+taxon_pages = {}
+taxon_regex = "(?:^|\W)[A-Z]{4}(?:^|\W)"
+p = re.compile(taxon_regex)
+for i in range(39, len(doc)):
+    page = doc.loadPage(i)
+    text = page.getText("text")
+    match = p.search(text)
+    if match:
+        taxon = ''.join(x for x in match.group().strip() if x.isalpha())
+        taxon_pages.setdefault(taxon, []).append(i)
+        print(taxon, i+1)
+        
+    # if match and match.group().find("TOME")==-1:
+    #     print("---------------------------------")
+    #     print(page)
+    #     print(match.group(), page.number)
+    #     taxon = ''.join(e for e in match.group() if e.isalnum())
+    #     i = 0
+    #     for img in doc.getPageImageList(page.number):
+    #         xref = img[0]
+    #         pix = fitz.Pixmap(doc, xref)
+    #         if pix.n >=5:        # CMYK: convert to RGB first
+    #             pix = fitz.Pixmap(fitz.csRGB, pix)
+    #         img = pix2np(pix)
+    #         if img.shape[0]>10 and img.shape[1]>10:
+    #             area = img.shape[0]*img.shape[1]
+    #             if img.shape[2]!=1:
+    #                 img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #             ret,img_thr = cv2.threshold(img_gs,254,255,cv2.THRESH_BINARY)
+    #             n = len(np.where(img_thr==255)[0])
+    #             if n<area*0.2:
+    #                 saveImg(img_gs, "tmp", taxon, i)
+    #                 i+=1
 
 # %%
