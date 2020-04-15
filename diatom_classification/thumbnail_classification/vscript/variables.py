645067dcb437c084e54cb55a8c0f8e8d1ce8846f
PierreFG <pierre.faure.giovagnoli@gmail.com>
2020-04-15 19:45:45 +0200

    k

--- diatom_classification/thumbnail_classification/vscript/variables.py
+++
@@ -1,4 +0,0 @@
-ROOT = "/mnt/nvme-storage/pfauregi"
-DATASET_PATH = ROOT+"/datasets/augmented/balanced"
-SELECTED_TAXONS="../selected_taxons.txt"
-LOG_DIR = "/mnt/nvme-storage/pfauregi/training/thumbails/train01/"
