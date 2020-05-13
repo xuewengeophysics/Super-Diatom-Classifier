11b035e3f622c92ff3b6c1f765f8ec736a252849
PierreFG <pierre.faure.giovagnoli@gmail.com>
2020-05-13 11:27:04 +0200

    k

--- diatom_classification/full_image_detection/artifical_dataset/scriptv/variables.py
+++
@@ -1,7 +0,0 @@
-#ROOT = "/mnt/balrog/pfauregi"
-ROOT = "/mnt/nvme-storage/pfauregi"
-DATASET_PATH = [ROOT+"/datasets/atlas/ra", ROOT+"/datasets/atlas/idf"]
-DATASET_DUST_PATH = [ROOT+"/datasets/dust01"]
-SAVE_PATH = ROOT+"/artificial_datasets/dataset03"
-#SAVE_PATH = ROOT+"/rw/dataset03"
-N_IMAGES = 20000
